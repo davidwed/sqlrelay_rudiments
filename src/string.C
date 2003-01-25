47a8c1f3ff5de8c1555cdfed8b79a8662baed5d4
David Muse <david.muse@firstworks.com>
2003-01-25 20:16:29 +0000

    *** empty log message ***

---
+++ src/string.C
@@ -0,0 +1,8 @@
+// Copyright (c) 2003 David Muse
+// See the COPYING file for more information
+
+#include <rudiments/string.h>
+#ifndef ENABLE_INLINES
+	#define inline
+	#include <rudiments/private/stringinlines.h>
+#endif
