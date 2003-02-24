14725a1b8f973479d356a4c73ae68db9a1813087
David Muse <david.muse@firstworks.com>
2003-02-24 04:14:17 +0000

    fixed rudimentsinlines which had a backwards ifdef

--- include/rudiments/private/rudimentsinlines.h
+++ include/rudiments/private/rudimentsinlines.h
@@ -1,7 +1,7 @@
 // Copyright (c) 1999-2002 David Muse
 // See the COPYING file for more information
 
-#ifndef ENABLE_INLINES
+#ifdef ENABLE_INLINES
 	#define RUDIMENTS_INLINE inline
 #else
 	#define RUDIMENTS_INLINE
