0afb58e64ed50c7662f35c98757e0a74000c9eeb
David Muse <david.muse@firstworks.com>
2003-04-15 18:14:12 +0000

    oops, forgot to add this file

---
+++ src/environment.C
@@ -0,0 +1,40 @@
+// Copyright (c) 2003 David Muse
+// See the COPYING file for more information
+
+#include <rudiments/environment.h>
+#ifndef ENABLE_INLINES
+	#define RUDIMENTS_INLINE
+	#include <rudiments/private/environmentinlines.h>
+#endif
+
+#if defined(HAVE_PUTENV) && !defined(HAVE_SETENV)
+
+#include <stdio.h>
+
+environment::~environment() {
+	for (namevaluepairslistnode *node=
+			(namevaluepairslistnode *)
+				envstrings.getList()->getNodeByIndex(0);
+			node;
+			node=(namevaluepairslistnode *)node->getNext()) {
+		delete[] node->getData()->getData();
+	}
+}
+
+int environment::setValue(const char *variable, const char *value) {
+	char	*pestr;
+	if (envstrings.getData((char *)variable,&pestr)) {
+		delete[] pestr;
+	}
+	pestr=new char[strlen(variable)+strlen(value)+2];
+	sprintf(pestr,"%s=%s",variable,value);
+	if (putenv(pestr)!=-1) {
+		envstrings.setData((char *)variable,pestr);
+		return 1;
+	} else {
+		delete[] pestr;
+		envstrings.removeData((char *)variable);
+		return 0;
+	}
+}
+#endif
