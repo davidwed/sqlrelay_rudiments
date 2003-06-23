7095ded50a054c10fcba1e3eff8018f1a5fda080
David Muse <david.muse@firstworks.com>
2003-06-23 16:12:50 +0000

    changed shmat() test from  shmat()>-1  to  shmat()!=-1

--- src/sharedmemory.C
+++ src/sharedmemory.C
@@ -27,8 +27,13 @@ int sharedmemory::create(key_t key, int size, mode_t permissions) {
 		// mark for removal
 		created=1;
 
-		// attach to the segment
+		// attach to the segment, remove the
+		// segment and return 0 on failure
 		shmptr=shmat(shmid,0,0);
+		if ((int)shmptr==-1) {
+			forceRemove();
+			return 0;
+		}
 
 		// init the segment to zero's
 		memset((void *)shmptr,0,size);
@@ -39,8 +44,25 @@ int sharedmemory::create(key_t key, int size, mode_t permissions) {
 }
 
 int sharedmemory::attach(key_t key) {
+
+	// shmat's documentation says something like...
+	// RETURN VALUE
+	//	On failure shmat returns -1 with errno indicating the error.
+	//	On success shmat returns the address of the attached shared
+	//	memory segment.
+	// So you might think, ok shmat()>-1 is success.  Ahh, but wait, shmat()
+	// returns a (void *) which doesn't really have an well defined size.
+	// So, it's possible for a memory address to be much bigger than the
+	// upper bound of whatever really big, signed integer your system might
+	// have and thus appear to be a negative number when cast to that type.
+	// This appears to happen on Solaris x86 all the time.  Also, compilers
+	// don't like it when you compare (void *)'s to numbers, so you have to
+	// cast it.  And you can't just cast it to an unsigned integer because
+	// it needs to be compared to -1.  So, we cast it to a signed integer
+	// to see if it's not -1, but allow that it could very well be less
+	// than -1 and still be valid.
 	return ((shmid=shmget(key,0,0))>-1 &&
-			(int)(shmptr=shmat(shmid,0,0))>-1);
+			(int)(shmptr=shmat(shmid,0,0))!=-1);
 }
 
 int sharedmemory::createOrAttach(key_t key, int size, mode_t permissions) {
@@ -51,8 +73,13 @@ int sharedmemory::createOrAttach(key_t key, int size, mode_t permissions) {
 		// mark for removal
 		created=1;
 
-		// attach to the segment
+		// attach to the segment, remove the
+		// segment and return 0 on failure
 		shmptr=shmat(shmid,0,0);
+		if ((int)shmptr==-1) {
+			forceRemove();
+			return 0;
+		}
 
 		// init the segment to zero's
 		memset((void *)shmptr,0,size);
@@ -60,9 +87,9 @@ int sharedmemory::createOrAttach(key_t key, int size, mode_t permissions) {
 		
 	} else if (errno==EEXIST && (shmid=shmget(key,0,permissions))>-1) {
 
-		// attach to the segment
+		// attach to the segment, return 1 on success and 0 on failure
 		shmptr=shmat(shmid,0,0);
-		return 1;
+		return ((int)shmptr!=-1);
 
 	}
 
