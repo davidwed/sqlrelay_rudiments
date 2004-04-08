9bbb93874f7886f6d74200ccfb7db6fa5476badd
David Muse <david.muse@firstworks.com>
2004-04-08 14:40:38 +0000

    changed several "char *blah=new char[...]; ... delete[] blah;"'s 	to "char blah[...];"'s

--- src/logger.C
+++ src/logger.C
@@ -87,47 +87,43 @@ char *logger::logHeader(const char *name) {
 }
 
 void logger::write(const char *header, int tabs, const char *string) const {
-	char	*logentry=new char[strlen(header)+3+tabs+strlen(string)+2+1];
+	char	logentry[strlen(header)+3+tabs+strlen(string)+2+1];
 	sprintf(logentry,"%s : ",header);
 	for (int i=0; i<tabs; i++) {
 		sprintf(logentry,"%s%c",logentry,'	');
 	}
 	sprintf(logentry,"%s%s\n\n",logentry,string);
 	write(logentry);
-	delete[] logentry;
 }
 
 void logger::write(const char *header, int tabs, char character) const {
-	char	*logentry=new char[strlen(header)+3+tabs+1+2+1];
+	char	logentry[strlen(header)+3+tabs+1+2+1];
 	sprintf(logentry,"%s : ",header);
 	for (int i=0; i<tabs; i++) {
 		sprintf(logentry,"%s%c",logentry,'	');
 	}
 	sprintf(logentry,"%s%c\n\n",logentry,character);
 	write(logentry);
-	delete[] logentry;
 }
 
 void logger::write(const char *header, int tabs, long number) const {
-	char	*logentry=new char[strlen(header)+3+tabs+20+2+1];
+	char	logentry[strlen(header)+3+tabs+20+2+1];
 	sprintf(logentry,"%s : ",header);
 	for (int i=0; i<tabs; i++) {
 		sprintf(logentry,"%s%c",logentry,'	');
 	}
 	sprintf(logentry,"%s%ld\n\n",logentry,number);
 	write(logentry);
-	delete[] logentry;
 }
 
 void logger::write(const char *header, int tabs, double number) const {
-	char	*logentry=new char[strlen(header)+3+tabs+21+2+1];
+	char	logentry[strlen(header)+3+tabs+21+2+1];
 	sprintf(logentry,"%s : ",header);
 	for (int i=0; i<tabs; i++) {
 		sprintf(logentry,"%s%c",logentry,'	');
 	}
 	sprintf(logentry,"%s%f\n\n",logentry,number);
 	write(logentry);
-	delete[] logentry;
 }
 
 void logger::write(const char *logentry) const {
