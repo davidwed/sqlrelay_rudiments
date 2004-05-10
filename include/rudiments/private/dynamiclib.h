f631e2e4a7bd3e40b149322b56e177923b477d7c
David Muse <david.muse@firstworks.com>
2004-05-10 07:12:30 +0000

    *** empty log message ***

---
+++ include/rudiments/private/dynamiclib.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2004 David Muse
+// See the COPYING file for more information
+
+	private:
+			void	*handle;
+
+			#ifdef RUDIMENTS_HAS_THREADS
+			static pthread_mutex_t	*errormutex;
+			#endif
