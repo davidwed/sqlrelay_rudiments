01e514f0b5f0812569fa3458e86e7e1ed6760781
David Muse <david.muse@firstworks.com>
2004-05-12 18:38:10 +0000

    oops, forgot to add math.C

---
+++ src/math.C
@@ -0,0 +1,8 @@
+// Copyright (c) 2004 David Muse
+// See the COPYING file for more information
+
+#include <rudiments/math.h>
+
+#ifndef ENABLE_RUDIMENTS_INLINES
+	#include <rudiments/private/mathinlines.h>
+#endif
