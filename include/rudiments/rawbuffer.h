e4399ccb32d1f3f2d4ea27d423c5577677b3d87c
David Muse <david.muse@firstworks.com>
2004-05-12 15:27:08 +0000

    added rawbuffer class

--- include/rudiments/rawbuffer.h
+++ include/rudiments/rawbuffer.h
@@ -18,42 +18,31 @@ class rawbuffer {
 	public:
 
 		static void	*copy(void *dest, const void *src, size_t size);
-					// memcpy
 		static void	*copyUntil(void *dest, const void *src,
 						int character, size_t size);
-					// memccpy
 
 		static void	*move(void *dest, const void *src, size_t size);
-					// memmove
 		static void	*moveSwapBytes(void *dest, const void *src,
 								size_t size);
-					// swab
 
 		static void	*set(void *dest, int c, size_t size);
-					// memset
 
 		static int	compare(const void *s1, const void *s2,
 								size_t size);
-					// memcmp
 
 		static void	*findFirst(const void *haystack, int needle,
 								size_t size);
-					// memchr
 		static void	*findLast(const void *haystack, int needle,
 								size_t size);
-					// memrchr
 
 		static void	*findFirst(const void *haystack,
 							size_t haystacksize,
 							const void *needle, 
 							size_t needleize);
-					// memmem
 		static void	*findLast(const void *haystack,
 							size_t haystacksize,
 							const void *needle, 
 							size_t needleize);
-
-	#include <rudiments/private/rawbuffer.h>
 };
 
 #endif
---
+++ src/rawbuffer.C
@@ -0,0 +1,78 @@
+#include <rudiments/rawbuffer.h>
+
+void *rawbuffer::copy(void *dest, const void *src, size_t size) {
+	return memcpy(dest,src,size);
+}
+
+void *rawbuffer::copyUntil(void *dest, const void *src,
+				int character, size_t size) {
+	return memccpy(dest,src,character,size);
+}
+
+void *rawbuffer::move(void *dest, const void *src, size_t size) {
+	return memmove(dest,src,size);
+}
+
+void *rawbuffer::moveSwapBytes(void *dest, const void *src, size_t size) {
+	swab(src,dest,size);
+	return dest;
+}
+
+void *rawbuffer::set(void *dest, int c, size_t size) {
+	return memset(dest,c,size);
+}
+
+int rawbuffer::compare(const void *s1, const void *s2, size_t size) {
+	return memcmp(s1,s2,size);
+}
+
+void *rawbuffer::findFirst(const void *haystack, int needle, size_t size) {
+	return memchr(haystack,needle,size);
+}
+
+void *rawbuffer::findLast(const void *haystack, int needle, size_t size) {
+	#ifdef HAVE_MEMRCHR
+		return memrchr(haystack,needle.size);
+	#else
+		unsigned char	realneedle=needle;
+		for (unsigned char *ptr=((unsigned char *)haystack)+
+							size;
+						ptr>=haystack; ptr--) {
+			
+			if (*ptr==realneedle) {
+				return (void *)ptr;
+			}
+		}
+		return NULL;
+	#endif
+}
+
+void *rawbuffer::findFirst(const void *haystack, size_t haystacksize,
+				const void *needle, size_t needlesize) {
+	#ifdef HAVE_MEMMEM
+		return memmem(haystack,haystacksize,needle,needlesize);
+	#else
+		unsigned char	*endptr=(unsigned char *)haystack+
+						haystacksize-needlesize;
+		for (unsigned char *ptr=(unsigned char *)haystack;
+						ptr<endptr; ptr++) {
+			if (!memcmp((void *)ptr,needle,needlesize)) {
+				return (void *)ptr;
+			}
+		}
+		return NULL;
+	#endif
+}
+
+void *rawbuffer::findLast(const void *haystack, size_t haystacksize,
+				const void *needle, size_t needlesize) {
+
+	for (unsigned char *ptr=((unsigned char *)haystack)+
+					haystacksize-needlesize;
+						ptr>=haystack; ptr--) {
+		if (!memcmp((void *)ptr,needle,needlesize)) {
+			return (void *)ptr;
+		}
+	}
+	return NULL;
+}
