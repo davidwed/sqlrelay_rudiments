aff4faeb036e3222d17b2c1405e399870c1335bc
David Muse <david.muse@firstworks.com>
2004-07-28 15:42:42 +0000

    *** empty log message ***

---
+++ src/container.C
@@ -0,0 +1,46 @@
+// Copyright (c) 2004 David Muse
+// See the COPYING file for more information
+
+#include <rudiments/container.h>
+
+container::container() {
+	copyreferences=false;
+	cascadeondelete=false;
+}
+
+container::~container() {
+}
+
+
+void container::copyReferences() {
+	copyreferences=true;
+}
+
+void container::dontCopyReferences() {
+	copyreferences=false;
+}
+
+void container::cascadeOnDelete() {
+	cascadeondelete=true;
+}
+
+void container::dontCascadeOnDelete() {
+	cascadeondelete=false;
+}
+
+
+bool container::save(filedescriptor *fd) {
+	return false;
+}
+
+bool container::save(unsigned char *buffer) {
+	return false;
+}
+
+bool container::load(filedescriptor *fd) {
+	return false;
+}
+
+bool container::load(unsigned char *buffer) {
+	return false;
+}
