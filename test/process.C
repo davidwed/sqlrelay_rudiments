5677f63a3d8542c0b418c745889707d03c8a81f5
David Muse <david.muse@firstworks.com>
2007-02-22 02:33:57 +0000

    added new tests

---
+++ test/process.C
@@ -0,0 +1,13 @@
+// Copyright (c) 2003  David Muse
+// See the file COPYING for more information
+
+#include <stdio.h>
+
+#ifdef RUDIMENTS_NAMESPACE
+using namespace rudiments;
+#endif
+
+int main(int argc, const char **argv) {
+
+	// FIXME: implement this...
+}
