36dce152edff37179b7b648b08ddaa92863817be
David Muse <david.muse@firstworks.com>
2011-07-16 01:04:43 +0000

    #250 added check for timespec in pthread

--- include/rudiments/private/snoozeincludes.h
+++ include/rudiments/private/snoozeincludes.h
@@ -11,7 +11,8 @@
 
 
 #if !defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC) && \
-	!defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_SIGINFO)
+	!defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_SIGINFO) && \
+	!defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_PTHREAD)
 struct timespec {
 	time_t	tv_sec;
 	long	tv_nsec;
