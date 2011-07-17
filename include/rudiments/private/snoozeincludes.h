3b7bb57875d1e210cfdfbd5d6acf17eedceb2a31
David Muse <david.muse@firstworks.com>
2011-07-17 00:47:05 +0000

    added sys/time.h no matter what so timeval would be defined

--- include/rudiments/private/snoozeincludes.h
+++ include/rudiments/private/snoozeincludes.h
@@ -3,23 +3,19 @@
 
 #include <rudiments/private/inttypes.h>
 
+// for timeval
+#include <sys/time.h>
+
 // for timespec
 #if defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_TIME_H)
 	#include <time.h>
-#elif defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_SYS_TIME_H)
-	#include <sys/time.h>
 #elif defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_SIGINFO)
 	#include <sys/siginfo.h>
 #elif defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_PTHREAD)
 	#include <pthread.h>
-#endif
-
-
-#if !defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC) && \
-	!defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_SIGINFO) && \
-	!defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_PTHREAD)
-struct timespec {
-	time_t	tv_sec;
-	long	tv_nsec;
-};
+#elif !defined(RUDIMENTS_HAVE_STRUCT_TIMESPEC_IN_SYS_TIME_H)
+	struct timespec {
+		time_t	tv_sec;
+		long	tv_nsec;
+	};
 #endif
