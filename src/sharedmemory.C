8500a151fe1ff9010d37260c22a191b69ce30e1c
David Muse <david.muse@firstworks.com>
2011-07-20 15:53:32 +0000

    #255 if shmget is undefined, sharedmemory methods set ENOSYS and return errors

--- src/sharedmemory.C
+++ src/sharedmemory.C
@@ -37,7 +37,12 @@ sharedmemory::~sharedmemory() {
 }
 
 bool sharedmemory::forceRemove() {
-	return shmControl(IPC_RMID,NULL);
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		return shmControl(IPC_RMID,NULL);
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 void sharedmemory::dontRemove() {
@@ -53,180 +58,256 @@ void *sharedmemory::getPointer() const {
 }
 
 bool sharedmemory::setUserId(uid_t uid) {
-	shmid_ds	setds;
-	setds.shm_perm.uid=uid;
-	return shmControl(IPC_SET,&setds);
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		shmid_ds	setds;
+		setds.shm_perm.uid=uid;
+		return shmControl(IPC_SET,&setds);
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool sharedmemory::setGroupId(gid_t gid) {
-	shmid_ds	setds;
-	setds.shm_perm.gid=gid;
-	return shmControl(IPC_SET,&setds);
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		shmid_ds	setds;
+		setds.shm_perm.gid=gid;
+		return shmControl(IPC_SET,&setds);
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool sharedmemory::setPermissions(mode_t permissions) {
-	shmid_ds	setds;
-	setds.shm_perm.mode=permissions;
-	return shmControl(IPC_SET,&setds);
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		shmid_ds	setds;
+		setds.shm_perm.mode=permissions;
+		return shmControl(IPC_SET,&setds);
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 uid_t sharedmemory::getUserId() {
-	shmid_ds	getds;
-	if (shmControl(IPC_STAT,&getds)) {
-		return getds.shm_perm.uid;
-	}
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		shmid_ds	getds;
+		if (shmControl(IPC_STAT,&getds)) {
+			return getds.shm_perm.uid;
+		}
+	#else
+		error::setErrorNumber(ENOSYS);
+	#endif
 	return 0;
 }
 
 gid_t sharedmemory::getGroupId() {
-	shmid_ds	getds;
-	if (shmControl(IPC_STAT,&getds)) {
-		return getds.shm_perm.gid;
-	}
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		shmid_ds	getds;
+		if (shmControl(IPC_STAT,&getds)) {
+			return getds.shm_perm.gid;
+		}
+	#else
+		error::setErrorNumber(ENOSYS);
+	#endif
 	return 0;
 }
 
 mode_t sharedmemory::getPermissions() {
-	shmid_ds	getds;
-	if (shmControl(IPC_STAT,&getds)) {
-		return getds.shm_perm.mode;
-	}
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		shmid_ds	getds;
+		if (shmControl(IPC_STAT,&getds)) {
+			return getds.shm_perm.mode;
+		}
+	#else
+		error::setErrorNumber(ENOSYS);
+	#endif
 	return 0;
 }
 
 bool sharedmemory::create(key_t key, size_t size, mode_t permissions) {
 
-	// create the shared memory segment
-	if ((pvt->_shmid=shmget(key,size,IPC_CREAT|IPC_EXCL|permissions))!=-1) {
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		// create the shared memory segment
+		if ((pvt->_shmid=shmget(key,size,
+				IPC_CREAT|IPC_EXCL|permissions))!=-1) {
 
-		// mark for removal
-		pvt->_created=true;
+			// mark for removal
+			pvt->_created=true;
 
-		// attach to the segment, remove the
-		// segment and return 0 on failure
-		pvt->_shmptr=shmAttach();
-		if (reinterpret_cast<long>(pvt->_shmptr)==-1) {
-			forceRemove();
-			return false;
+			// attach to the segment, remove the
+			// segment and return 0 on failure
+			pvt->_shmptr=shmAttach();
+			if (reinterpret_cast<long>(pvt->_shmptr)==-1) {
+				forceRemove();
+				return false;
+			}
+
+			// init the segment to zero's
+			rawbuffer::zero(pvt->_shmptr,size);
+			return true;
 		}
-
-		// init the segment to zero's
-		rawbuffer::zero(pvt->_shmptr,size);
-		return true;
-	}
-
+	#else
+		error::setErrorNumber(ENOSYS);
+	#endif
 	return false;
 }
 
 bool sharedmemory::attach(key_t key) {
 
-	// shmat's documentation says something like...
-	// RETURN VALUE
-	//	On failure shmat returns -1 with errno indicating the error.
-	//	On success shmat returns the address of the attached shared
-	//	memory segment.
-	// So you might think, ok shmat()>-1 is success.  Ahh, but wait, shmat()
-	// returns a (void *) which doesn't really have an well defined size.
-	// So, it's possible for a memory address to be much bigger than the
-	// upper bound of whatever really big, signed integer your system might
-	// have and thus appear to be a negative number when cast to that type.
-	// This appears to happen on Solaris x86 all the time.  Also, compilers
-	// don't like it when you compare (void *)'s to numbers, so you have to
-	// cast it.  And you can't just cast it to an unsigned integer because
-	// it needs to be compared to -1.  So, we cast it to a signed integer
-	// to see if it's not -1, but allow that it could very well be less
-	// than -1 and still be valid.
-	return ((pvt->_shmid=shmGet(key,0,0))!=-1 &&
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		// shmat's documentation says something like...
+		// RETURN VALUE
+		//	On failure shmat returns -1 with errno
+		//		indicating the error.
+		//	On success shmat returns the address of the
+		//		attached shared memory segment.
+		// So you might think, ok shmat()>-1 is success.  Ahh, but wait,
+		// shmat()returns a (void *) which doesn't really have an well
+		// defined size.  So, it's possible for a memory address to be
+		// much bigger than the upper bound of whatever really big,
+		// signed integer your system might have and thus appear to be
+		// a negative number when cast to that type.  This appears to
+		// happen on Solaris x86 all the time.  Also, compilers don't
+		// like it when you compare (void *)'s to numbers, so you have
+		// to cast it.  And you can't just cast it to an unsigned
+		// integer because it needs to be compared to -1.  So, we cast
+		// it to a signed integer to see if it's not -1, but allow that
+		// it could very well be less than -1 and still be valid.
+		return ((pvt->_shmid=shmGet(key,0,0))!=-1 &&
 			reinterpret_cast<long>(pvt->_shmptr=shmAttach())!=-1);
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool sharedmemory::createOrAttach(key_t key, size_t size, mode_t permissions) {
 
-	// create the shared memory segment
-	if ((pvt->_shmid=shmGet(key,size,IPC_CREAT|IPC_EXCL|permissions))!=-1) {
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		// create the shared memory segment
+		if ((pvt->_shmid=shmGet(key,size,
+				IPC_CREAT|IPC_EXCL|permissions))!=-1) {
 
-		// mark for removal
-		pvt->_created=true;
+			// mark for removal
+			pvt->_created=true;
 
-		// attach to the segment, remove the
-		// segment and return 0 on failure
-		pvt->_shmptr=shmAttach();
-		if (reinterpret_cast<long>(pvt->_shmptr)==-1) {
-			forceRemove();
-			return false;
-		}
+			// attach to the segment, remove the
+			// segment and return 0 on failure
+			pvt->_shmptr=shmAttach();
+			if (reinterpret_cast<long>(pvt->_shmptr)==-1) {
+				forceRemove();
+				return false;
+			}
 
-		// init the segment to zero's
-		rawbuffer::zero(pvt->_shmptr,size);
-		return true;
+			// init the segment to zero's
+			rawbuffer::zero(pvt->_shmptr,size);
+			return true;
 		
-	} else if (error::getErrorNumber()==EEXIST &&
-			(pvt->_shmid=shmGet(key,0,permissions))!=-1) {
+		} else if (error::getErrorNumber()==EEXIST &&
+				(pvt->_shmid=shmGet(key,0,permissions))!=-1) {
 
-		// attach to the segment, return 1 on success and 0 on failure
-		pvt->_shmptr=shmAttach();
-		return (reinterpret_cast<long>(pvt->_shmptr)!=-1);
+			// attach to the segment,
+			// return 1 on success and 0 on failure
+			pvt->_shmptr=shmAttach();
+			return (reinterpret_cast<long>(pvt->_shmptr)!=-1);
 
-	}
-
+		}
+	#else
+		error::setErrorNumber(ENOSYS);
+	#endif
 	return false;
 }
 
 const char *sharedmemory::getUserName() {
-	shmid_ds	getds;
-	char		*name;
-	if (shmControl(IPC_STAT,&getds) &&
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		shmid_ds	getds;
+		char		*name;
+		if (shmControl(IPC_STAT,&getds) &&
 			passwdentry::getName(getds.shm_perm.uid,&name)) {
-		return name;
-	}
+			return name;
+		}
+	#else
+		error::setErrorNumber(ENOSYS);
+	#endif
 	return NULL;
 }
 
 const char *sharedmemory::getGroupName() {
-	shmid_ds	getds;
-	char		*name;
-	if (shmControl(IPC_STAT,&getds) &&
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		shmid_ds	getds;
+		char		*name;
+		if (shmControl(IPC_STAT,&getds) &&
 			groupentry::getName(getds.shm_perm.gid,&name)) {
-		return name;
-	}
+			return name;
+		}
+	#else
+		error::setErrorNumber(ENOSYS);
+	#endif
 	return NULL;
 }
 
 bool sharedmemory::setUserName(const char *username) {
-	uid_t	userid;
-	return (passwdentry::getUserId(username,&userid) &&
-						setUserId(userid));
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		uid_t	userid;
+		return (passwdentry::getUserId(username,&userid) &&
+							setUserId(userid));
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool sharedmemory::setGroupName(const char *groupname) {
-	gid_t	groupid;
-	return (groupentry::getGroupId(groupname,&groupid) &&
-						setGroupId(groupid));
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		gid_t	groupid;
+		return (groupentry::getGroupId(groupname,&groupid) &&
+							setGroupId(groupid));
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 int sharedmemory::shmGet(key_t key, size_t size, int shmflag) {
-	int	result;
-	do {
-		result=shmget(key,size,shmflag);
-	} while (result==-1 && error::getErrorNumber()==EINTR);
-	return result;
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		int	result;
+		do {
+			result=shmget(key,size,shmflag);
+		} while (result==-1 && error::getErrorNumber()==EINTR);
+		return result;
+	#else
+		error::setErrorNumber(ENOSYS);
+		return -1;
+	#endif
 }
 
 void *sharedmemory::shmAttach() {
-	void	*result;
-	do {
-		result=shmat(pvt->_shmid,0,0);
-	} while (reinterpret_cast<long>(result)==-1 &&
-			error::getErrorNumber()==EINTR);
-	return result;
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		void	*result;
+		do {
+			result=shmat(pvt->_shmid,0,0);
+		} while (reinterpret_cast<long>(result)==-1 &&
+				error::getErrorNumber()==EINTR);
+		return result;
+	#else
+		error::setErrorNumber(ENOSYS);
+		return NULL;
+	#endif
 }
 
 bool sharedmemory::shmControl(int cmd, shmid_ds *buf) {
-	int	result;
-	do {
-		result=shmctl(pvt->_shmid,cmd,buf);
-	} while (result==-1 && error::getErrorNumber()==EINTR);
-	return !result;
+	#ifdef RUDIMENTS_HAVE_SHMGET
+		int	result;
+		do {
+			result=shmctl(pvt->_shmid,cmd,buf);
+		} while (result==-1 && error::getErrorNumber()==EINTR);
+		return !result;
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 #ifdef RUDIMENTS_NAMESPACE
