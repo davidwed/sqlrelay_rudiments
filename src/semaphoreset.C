601ae591f28086e70efa418e9104f2ae159516f3
David Muse <david.muse@firstworks.com>
2011-07-27 19:42:26 +0000

    #261 added test for CreateSemaphore added placeholders for win32 code

--- src/semaphoreset.C
+++ src/semaphoreset.C
@@ -31,85 +31,63 @@
 	};
 #endif
 
-// for platforms that don't support semctl, these won't be defined either
-#ifndef RUDIMENTS_HAVE_SEMCTL
-	#define IPC_RMID	0
-	#define IPC_SET		1
-	#define IPC_STAT	2
-	#define GETVAL		3
-	#define SETVAL		4
-	#define GETZCNT		5
-	#define GETNCNT		6
-
-	struct ipc_perm {
-		key_t		__key;
-		uid_t		uid;
-		gid_t		gid;
-		uid_t		cuid;
-		uid_t		cgid;
-		uint16_t	mode;
-		uint16_t	__seq;
-	};
-
-	struct semid_ds {
-		struct ipc_perm	sem_perm;
-		time_t		sem_otime;
-		time_t		sem_ctime;
-		uint16_t	sem_nsems;
-	};
-#endif
-
-// for platforms that don't support semget, these won't be defined either
-#ifndef RUDIMENTS_HAVE_SEMGET
-	#define IPC_CREAT	1
-	#define IPC_EXCL	2
-#endif
-
-// for platforms that don't support semop, these won't be defined either
-#ifndef RUDIMENTS_HAVE_SEMOP
-	#define SEM_UNDO 	1
-#endif
-
-
 #ifdef RUDIMENTS_NAMESPACE
 namespace rudiments {
 #endif
 
 class semaphoresetprivate {
 	public:
-			int	_semid;
-			bool	_created;
-			int	_semcount;
-		struct	sembuf	**_waitop;
-		struct	sembuf	**_waitwithundoop;
-		struct	sembuf	**_signalop;
-		struct	sembuf	**_signalwithundoop;
+		int	_semid;
+		bool	_created;
+		int	_semcount;
+		#if defined(RUDIMENTS_HAVE_SEMGET)
+			struct	sembuf	**_waitop;
+			struct	sembuf	**_waitwithundoop;
+			struct	sembuf	**_signalop;
+			struct	sembuf	**_signalwithundoop;
+		#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+			HANDLE	*_semaphores;
+		#endif
 };
 
 semaphoreset::semaphoreset() {
 	pvt=new semaphoresetprivate;
 	pvt->_semid=-1;
 	pvt->_created=false;
-	pvt->_waitop=NULL;
-	pvt->_waitwithundoop=NULL;
-	pvt->_signalop=NULL;
-	pvt->_signalwithundoop=NULL;
+	pvt->_semcount=0;
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		pvt->_waitop=NULL;
+		pvt->_waitwithundoop=NULL;
+		pvt->_signalop=NULL;
+		pvt->_signalwithundoop=NULL;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		pvt->_semaphores=NULL;
+	#endif
 }
 
 semaphoreset::~semaphoreset() {
 
-	if (pvt->_waitop) {
-		for (int i=0; i<pvt->_semcount; i++) {
-			delete[] pvt->_waitop[i];
-			delete[] pvt->_waitwithundoop[i];
-			delete[] pvt->_signalop[i];
-			delete[] pvt->_signalwithundoop[i];
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		if (pvt->_waitop) {
+			for (int i=0; i<pvt->_semcount; i++) {
+				delete[] pvt->_waitop[i];
+				delete[] pvt->_waitwithundoop[i];
+				delete[] pvt->_signalop[i];
+				delete[] pvt->_signalwithundoop[i];
+			}
+			delete[] pvt->_waitop;
+			delete[] pvt->_waitwithundoop;
+			delete[] pvt->_signalop;
+			delete[] pvt->_signalwithundoop;
 		}
-		delete[] pvt->_waitop;
-		delete[] pvt->_waitwithundoop;
-		delete[] pvt->_signalop;
-		delete[] pvt->_signalwithundoop;
-	}
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		if (pvt->_semaphores) {
+			for (int i=0; i<pvt->_semcount; i++) {
+				CloseHandle(pvt->_semaphores[i]);
+			}
+			delete[] pvt->_semaphores;
+		}
+	#endif
 
 	if (pvt->_created) {
 		forceRemove();
@@ -119,8 +97,15 @@ semaphoreset::~semaphoreset() {
 }
 
 bool semaphoreset::forceRemove() {
-	semun	semctlun;
-	return !semControl(pvt,0,IPC_RMID,&semctlun);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semun	semctlun;
+		return !semControl(pvt,0,IPC_RMID,&semctlun);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 void semaphoreset::dontRemove() {
@@ -132,245 +117,381 @@ int semaphoreset::getId() const {
 }
 
 bool semaphoreset::wait(int index) {
-	return semOp(pvt->_waitop[index]);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		return semOp(pvt->_waitop[index]);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::wait(int index, long seconds, long nanoseconds) {
-	timespec	ts;
-	ts.tv_sec=seconds;
-	ts.tv_nsec=nanoseconds;
-	return semTimedOp(pvt->_waitop[index],&ts);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		timespec	ts;
+		ts.tv_sec=seconds;
+		ts.tv_nsec=nanoseconds;
+		return semTimedOp(pvt->_waitop[index],&ts);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::waitWithUndo(int index) {
-	return semOp(pvt->_waitwithundoop[index]);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		return semOp(pvt->_waitwithundoop[index]);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::waitWithUndo(int index, long seconds, long nanoseconds) {
-	timespec	ts;
-	ts.tv_sec=seconds;
-	ts.tv_nsec=nanoseconds;
-	return semTimedOp(pvt->_waitwithundoop[index],&ts);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		timespec	ts;
+		ts.tv_sec=seconds;
+		ts.tv_nsec=nanoseconds;
+		return semTimedOp(pvt->_waitwithundoop[index],&ts);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::signal(int index) {
-	return semOp(pvt->_signalop[index]);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		return semOp(pvt->_signalop[index]);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::signalWithUndo(int index) {
-	return semOp(pvt->_signalwithundoop[index]);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		return semOp(pvt->_signalwithundoop[index]);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 int semaphoreset::getValue(int index) {
-	semun	semctlun;
-	return semControl(pvt,index,GETVAL,&semctlun);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semun	semctlun;
+		return semControl(pvt,index,GETVAL,&semctlun);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::setValue(int index, int value) {
-	semun	semctlun;
-	semctlun.val=value;
-	return !semControl(pvt,index,SETVAL,&semctlun);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semun	semctlun;
+		semctlun.val=value;
+		return !semControl(pvt,index,SETVAL,&semctlun);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 int semaphoreset::getWaitingForZero(int index) {
-	semun	semctlun;
-	return semControl(pvt,index,GETZCNT,&semctlun);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semun	semctlun;
+		return semControl(pvt,index,GETZCNT,&semctlun);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 int semaphoreset::getWaitingForIncrement(int index) {
-	semun	semctlun;
-	return semControl(pvt,index,GETNCNT,&semctlun);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semun	semctlun;
+		return semControl(pvt,index,GETNCNT,&semctlun);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::create(key_t key, mode_t permissions, 
 					int semcount, const int *values) {
 
-	pvt->_semcount=semcount;
+	#if !defined(RUDIMENTS_HAVE_SEMGET) && \
+		!defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
 
-	// create the semaphore
-	if ((pvt->_semid=semGet(key,semcount,
-				IPC_CREAT|IPC_EXCL|permissions))!=-1) {
+		error::setErrorNumber(ENOSYS);
+		return false;
 
-		// if creation succeeded, initialize the semaphore
-		if (values) {
-			for (int i=0; i<semcount; i++) {
-				setValue(i,values[i]);
+	#else
+
+		pvt->_semcount=semcount;
+
+		// create the semaphore
+		#if defined(RUDIMENTS_HAVE_SEMGET)
+			if ((pvt->_semid=semGet(key,semcount,
+					IPC_CREAT|IPC_EXCL|permissions))!=-1) {
+		#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+			#error not sure what to do here...
+			if (true) {
+		#endif
+	
+			// if creation succeeded, initialize the semaphore
+			if (values) {
+				for (int i=0; i<semcount; i++) {
+					setValue(i,values[i]);
+				}
 			}
+	
+			// mark for removal
+			pvt->_created=true;
+	
+			#ifdef RUDIMENTS_HAVE_SEMGET
+				// create the signal/wait operations
+				createOperations();
+			#endif
+	
+			return true;
 		}
 
-		// mark for removal
-		pvt->_created=true;
-
-		createOperations();
-		return true;
-	}
-
-	return false;
+		return false;
+	#endif
 }
 
 bool semaphoreset::attach(key_t key, int semcount) {
 
 	pvt->_semcount=semcount;
 
+	// attach to the semaphore
+	#if defined(RUDIMENTS_HAVE_SEMGET)
 	if ((pvt->_semid=semGet(key,semcount,0))!=-1) {
 		createOperations();
 		return true;
 	}
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+	#endif
 
 	return false;
 }
 
 bool semaphoreset::createOrAttach(key_t key, mode_t permissions, 
 					int semcount, const int *values) {
-
-	pvt->_semcount=semcount;
-
-	// create the semaphore
-	if ((pvt->_semid=semGet(key,semcount,
-				IPC_CREAT|IPC_EXCL|permissions))!=-1) {
-
-		// if creation succeeded, initialize the semaphore
-		if (values) {
-			for (int i=0; i<semcount; i++) {
-				setValue(i,values[i]);
-			}
-		}
-
-		// mark for removal
-		pvt->_created=true;
-		
-	} else if (!(error::getErrorNumber()==EEXIST && 
-			(pvt->_semid=semGet(key,semcount,permissions))!=-1)) {
-
-		return false;
+	if (create(key,permissions,semcount,values)) {
+		return true;
 	}
-	createOperations();
-	return true;
+	if (error::getErrorNumber()==EEXIST) {
+		return attach(key,semcount);
+	}
+	return false;
 }
 
 void semaphoreset::createOperations() {
 
-	pvt->_waitop=new sembuf *[pvt->_semcount];
-	pvt->_waitwithundoop=new sembuf *[pvt->_semcount];
-	pvt->_signalop=new sembuf *[pvt->_semcount];
-	pvt->_signalwithundoop=new sembuf *[pvt->_semcount];
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		pvt->_waitop=new sembuf *[pvt->_semcount];
+		pvt->_waitwithundoop=new sembuf *[pvt->_semcount];
+		pvt->_signalop=new sembuf *[pvt->_semcount];
+		pvt->_signalwithundoop=new sembuf *[pvt->_semcount];
 
-	for (int i=0; i<pvt->_semcount; i++) {
+		for (int i=0; i<pvt->_semcount; i++) {
 
-		// wait without undo
-		pvt->_waitop[i]=new sembuf[1];
-		pvt->_waitop[i][0].sem_num=(short)i;
-		pvt->_waitop[i][0].sem_op=-1;
-		pvt->_waitop[i][0].sem_flg=0;
+			// wait without undo
+			pvt->_waitop[i]=new sembuf[1];
+			pvt->_waitop[i][0].sem_num=(short)i;
+			pvt->_waitop[i][0].sem_op=-1;
+			pvt->_waitop[i][0].sem_flg=0;
 
-		// wait with undo
-		pvt->_waitwithundoop[i]=new sembuf[1];
-		pvt->_waitwithundoop[i][0].sem_num=(short)i;
-		pvt->_waitwithundoop[i][0].sem_op=-1;
-		pvt->_waitwithundoop[i][0].sem_flg=SEM_UNDO;
+			// wait with undo
+			pvt->_waitwithundoop[i]=new sembuf[1];
+			pvt->_waitwithundoop[i][0].sem_num=(short)i;
+			pvt->_waitwithundoop[i][0].sem_op=-1;
+			pvt->_waitwithundoop[i][0].sem_flg=SEM_UNDO;
 
-		// signal without undo
-		pvt->_signalop[i]=new sembuf[1];
-		pvt->_signalop[i][0].sem_num=(short)i;
-		pvt->_signalop[i][0].sem_op=1;
-		pvt->_signalop[i][0].sem_flg=0;
+			// signal without undo
+			pvt->_signalop[i]=new sembuf[1];
+			pvt->_signalop[i][0].sem_num=(short)i;
+			pvt->_signalop[i][0].sem_op=1;
+			pvt->_signalop[i][0].sem_flg=0;
 
-		// signal with undo
-		pvt->_signalwithundoop[i]=new sembuf[1];
-		pvt->_signalwithundoop[i][0].sem_num=(short)i;
-		pvt->_signalwithundoop[i][0].sem_op=1;
-		pvt->_signalwithundoop[i][0].sem_flg=SEM_UNDO;
-	}
+			// signal with undo
+			pvt->_signalwithundoop[i]=new sembuf[1];
+			pvt->_signalwithundoop[i][0].sem_num=(short)i;
+			pvt->_signalwithundoop[i][0].sem_op=1;
+			pvt->_signalwithundoop[i][0].sem_flg=SEM_UNDO;
+		}
+	#endif
 }
 
 bool semaphoreset::setUserId(uid_t uid) {
-	semid_ds	setds;
-	setds.sem_perm.uid=uid;
-	semun	semctlun;
-	semctlun.buf=&setds;
-	return !semControl(pvt,0,IPC_SET,&semctlun);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semid_ds	setds;
+		setds.sem_perm.uid=uid;
+		semun	semctlun;
+		semctlun.buf=&setds;
+		return !semControl(pvt,0,IPC_SET,&semctlun);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::setGroupId(gid_t gid) {
-	semid_ds	setds;
-	setds.sem_perm.gid=gid;
-	semun	semctlun;
-	semctlun.buf=&setds;
-	return !semControl(pvt,0,IPC_SET,&semctlun);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semid_ds	setds;
+		setds.sem_perm.gid=gid;
+		semun	semctlun;
+		semctlun.buf=&setds;
+		return !semControl(pvt,0,IPC_SET,&semctlun);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 bool semaphoreset::setUserName(const char *username) {
 	uid_t	userid;
 	return passwdentry::getUserId(username,&userid) &&
-			setUserId(userid);
+					setUserId(userid);
 }
 
 bool semaphoreset::setGroupName(const char *groupname) {
 	gid_t	groupid;
 	return groupentry::getGroupId(groupname,&groupid) &&
-			setGroupId(groupid);
+					setGroupId(groupid);
 }
 
 bool semaphoreset::setPermissions(mode_t permissions) {
-	semid_ds	setds;
-	setds.sem_perm.mode=permissions;
-	semun	semctlun;
-	semctlun.buf=&setds;
-	return !semControl(pvt,0,IPC_SET,&semctlun);
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semid_ds	setds;
+		setds.sem_perm.mode=permissions;
+		semun	semctlun;
+		semctlun.buf=&setds;
+		return !semControl(pvt,0,IPC_SET,&semctlun);
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 const char *semaphoreset::getUserName() {
-	semid_ds	getds;
-	semun		semctlun;
-	semctlun.buf=&getds;
-	char		*name;
-	if (!semControl(pvt,0,IPC_STAT,&semctlun) &&
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semid_ds	getds;
+		semun		semctlun;
+		semctlun.buf=&getds;
+		char		*name;
+		if (!semControl(pvt,0,IPC_STAT,&semctlun) &&
 			passwdentry::getName(getds.sem_perm.uid,&name)) {
-		return name;
-	}
-	return NULL;
+			return name;
+		}
+		return NULL;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 uid_t semaphoreset::getUserId() {
-	semid_ds	getds;
-	semun		semctlun;
-	semctlun.buf=&getds;
-	if (!semControl(pvt,0,IPC_STAT,&semctlun)) {
-		return (short)getds.sem_perm.uid;
-	}
-	return 0;
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semid_ds	getds;
+		semun		semctlun;
+		semctlun.buf=&getds;
+		if (!semControl(pvt,0,IPC_STAT,&semctlun)) {
+			return (short)getds.sem_perm.uid;
+		}
+		return 0;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 const char *semaphoreset::getGroupName() {
-	semid_ds	getds;
-	semun		semctlun;
-	semctlun.buf=&getds;
-	char		*name;
-	if (!semControl(pvt,0,IPC_STAT,&semctlun) &&
-			groupentry::getName(getds.sem_perm.gid,&name)) {
-		return name;
-	}
-	return NULL;
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semid_ds	getds;
+		semun		semctlun;
+		semctlun.buf=&getds;
+		char		*name;
+		if (!semControl(pvt,0,IPC_STAT,&semctlun) &&
+				groupentry::getName(getds.sem_perm.gid,&name)) {
+			return name;
+		}
+		return NULL;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 gid_t semaphoreset::getGroupId() {
-	semid_ds	getds;
-	semun		semctlun;
-	semctlun.buf=&getds;
-	if (!semControl(pvt,0,IPC_STAT,&semctlun)) {
-		return (short)getds.sem_perm.gid;
-	}
-	return 0;
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semid_ds	getds;
+		semun		semctlun;
+		semctlun.buf=&getds;
+		if (!semControl(pvt,0,IPC_STAT,&semctlun)) {
+			return (short)getds.sem_perm.gid;
+		}
+		return 0;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 mode_t semaphoreset::getPermissions() {
-	semid_ds	getds;
-	semun		semctlun;
-	semctlun.buf=&getds;
-	if (!semControl(pvt,0,IPC_STAT,&semctlun)) {
-		return getds.sem_perm.mode;
-	}
-	return 0;
+	#if defined(RUDIMENTS_HAVE_SEMGET)
+		semid_ds	getds;
+		semun		semctlun;
+		semctlun.buf=&getds;
+		if (!semControl(pvt,0,IPC_STAT,&semctlun)) {
+			return getds.sem_perm.mode;
+		}
+		return 0;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
+	#else
+		error::setErrorNumber(ENOSYS);
+		return false;
+	#endif
 }
 
 int semaphoreset::semGet(key_t key, int nsems, int semflg) {
@@ -380,6 +501,8 @@ int semaphoreset::semGet(key_t key, int nsems, int semflg) {
 			result=semget(key,nsems,semflg);
 		} while (result==-1 && error::getErrorNumber()==EINTR);
 		return result;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
 	#else
 		error::setErrorNumber(ENOSYS);
 		return false;
@@ -388,12 +511,14 @@ int semaphoreset::semGet(key_t key, int nsems, int semflg) {
 
 int semaphoreset::semControl(semaphoresetprivate *pvt, int semnum,
 						int cmd, semun *semctlun) {
-	#if defined(RUDIMENTS_HAVE_SEMCTL)
+	#if defined(RUDIMENTS_HAVE_SEMGET)
 		int	result;
 		do {
 			result=semctl(pvt->_semid,semnum,cmd,*semctlun);
 		} while (result==-1 && error::getErrorNumber()==EINTR);
 		return result;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
 	#else
 		error::setErrorNumber(ENOSYS);
 		return -1;
@@ -401,12 +526,14 @@ int semaphoreset::semControl(semaphoresetprivate *pvt, int semnum,
 }
 
 bool semaphoreset::semOp(struct sembuf *sops) {
-	#if defined(RUDIMENTS_HAVE_SEMOP)
+	#if defined(RUDIMENTS_HAVE_SEMGET)
 		int	result;
 		do {
 			result=semop(pvt->_semid,sops,1);
 		} while (result==-1 && error::getErrorNumber()==EINTR);
 		return !result;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
 	#else
 		error::setErrorNumber(ENOSYS);
 		return false;
@@ -420,6 +547,8 @@ bool semaphoreset::semTimedOp(struct sembuf *sops, timespec *ts) {
 			result=semtimedop(pvt->_semid,sops,1,ts);
 		} while (result==-1 && error::getErrorNumber()==EINTR);
 		return !result;
+	#elif defined(RUDIMENTS_HAVE_CREATESEMAPHORE)
+		#error not sure what to do here...
 	#else
 		error::setErrorNumber(ENOSYS);
 		return false;
