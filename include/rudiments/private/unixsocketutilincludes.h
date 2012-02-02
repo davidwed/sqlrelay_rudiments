c6a68f6145c201501fcb6ab55a86ec0643f07bed
David Muse <david.muse@firstworks.com>
2012-02-02 08:32:41 +0000

    *** empty log message ***

--- include/rudiments/private/unixsocketutilincludes.h
+++ include/rudiments/private/unixsocketutilincludes.h
@@ -3,14 +3,6 @@
 
 #include <rudiments/private/inttypes.h>
 
-#include <sys/types.h>
-#ifdef RUDIMENTS_HAVE_SYS_PARAM_H
-	#include <sys/param.h>
-#endif
-#ifdef RUDIMENTS_HAVE_SYS_UN_H
-	#include <sys/un.h>
-#endif
-
 #ifndef RUDIMENTS_HAVE_SOCKADDR_UN
 	#ifdef RUDIMENTS_HAVE_LIMITS_H
 		// for PATH_MAX
@@ -21,4 +13,12 @@
 		int16_t	sun_family;
 		char	sun_path[PATH_MAX];
 	};
+#endif
+
+#include <sys/types.h>
+#ifdef RUDIMENTS_HAVE_SYS_PARAM_H
+	#include <sys/param.h>
+#endif
+#ifdef RUDIMENTS_HAVE_SYS_UN_H
+	#include <sys/un.h>
 #endif
