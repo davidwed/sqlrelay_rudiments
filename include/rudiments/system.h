6d8b17de44be6b036357d90d4e0401fe4df7008b
David Muse <david.muse@firstworks.com>
2012-08-24 17:43:19 +0000

    *** empty log message ***

--- include/rudiments/system.h
+++ include/rudiments/system.h
@@ -14,24 +14,6 @@ namespace rudiments {
 
 class systemprivate;
 
-// wrap:
-//	unistd.h - sysconf()
-//			confstr() - use sysctl() on bsd
-//			gethostid(),sethostid() - use sysctl() on bsd
-//			sync()
-//	sys/raw.h - structs/defines for /dev/rawctl ioctl()'s
-//	not in solaris - 
-//		sys/sysctl.h - sysctl()
-//	linux only - 
-//		sys/kdaemon.h - bdflush()
-//		sys/klog.h - klogctl()
-//		sys/personality.h - personality() - set linux kernel personality
-//		sys/reboot.h - reboot()
-//		sys/swap.h - swapon()/swapoff()
-//		sys/sysinfo.h - functions for gettting overall system statistics
-//				sysinfo(),
-//				get_nprocs_conf(),get_nprocs(),
-//				get_phys_pages,get_avphys_pages()
 class RUDIMENTS_DLLSPEC system {
 	public:
 
