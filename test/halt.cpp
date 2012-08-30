43c0b92af338438e6c80f30cfc2ec86c27a055e4
David Muse <david.muse@firstworks.com>
2012-08-30 02:00:40 +0000

    #1034 added tests for halt, reboot and shutdown

---
+++ test/halt.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2012  David Muse
+// See the file COPYING for more information
+
+#include <rudiments/system.h>
+#include <rudiments/error.h>
+#include <stdio.h>
+
+#ifdef RUDIMENTS_NAMESPACE
+using namespace rudiments;
+#endif
+
+int main(int argc, const char **argv) {
+	if (!system::halt()) {
+		printf("halt failed: %s\n",error::getErrorString());
+	}
+}
