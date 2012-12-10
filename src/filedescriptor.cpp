5605cbf61f397f16feb395d81c19265c2956faf8
David Muse <david.muse@firstworks.com>
2012-12-10 19:36:26 +0000

    #1383 fancy-coded arond the typed-punned pointer warning

--- src/filedescriptor.cpp
+++ src/filedescriptor.cpp
@@ -1768,7 +1768,27 @@ bool filedescriptor::passFileDescriptor(int32_t fd) const {
 		cmptr->cmsg_level=SOL_SOCKET;
 		cmptr->cmsg_type=SCM_RIGHTS;
 		cmptr->cmsg_len=CMSG_LEN(sizeof(int32_t));
-		*(reinterpret_cast<int32_t *>(CMSG_DATA(cmptr)))=fd;
+
+		// Why this weirdness?
+		//
+		// You'd think that this would be simpler:
+		// 	*((int32_t *)CMSG_DATA(cmptr))=fd;
+		// and should work as CMSG_DATA(cmptr) just points to a generic
+		// buffer.
+		//
+		// The data type of that buffer varies from system to system
+		// though and on some systems, some versions of g++ will throw
+		// a warning like: "dereferencing type-punned pointer will break
+		// strict-aliasing rules" if you cast and then assign directly
+		// in one statement, presumably because it can't tell that it's
+		// a generic buffer, or how big it is, and worries that you
+		// might overrun it.
+		//
+		// What we want to do is safe though, and for some reason the
+		// compiler doesn't mind if you split it up like this, so we
+		// will.
+		int32_t	*data=(int32_t *)CMSG_DATA(cmptr);
+		*data=fd;
 	#else
 		// old-style: the descriptor is passed in the accrights...
 		messageheader.msg_accrights=(caddr_t)&fd;
@@ -1860,8 +1880,27 @@ bool filedescriptor::receiveFileDescriptor(int32_t *fd) const {
 				cmptr->cmsg_level==SOL_SOCKET &&
 				cmptr->cmsg_type==SCM_RIGHTS) {
 
-			// if we got good data, set the descriptor and return
-			*fd=*(reinterpret_cast<int32_t *>(CMSG_DATA(cmptr)));
+			// Why this weirdness?
+			//
+			// You'd think that this would be simpler:
+			// 	*fd=*((int32_t *)CMSG_DATA(cmptr));
+			// and should work as CMSG_DATA(cmptr) just points to a
+			// generic buffer.
+			//
+			// The data type of that buffer varies from system to
+			// system though and on some systems, some versions of
+			// g++ will throw a warning like: "dereferencing
+			// type-punned pointer will break strict-aliasing rules"
+			// if you cast and then dereference directly in one
+			// statement, presumably because it can't tell that
+			// it's a generic buffer, or how big it is, and worries
+			// that you might address past the end of it.
+			//
+			// What we want to do is safe though, and for some
+			// reason the compiler doesn't mind if you split it up
+			// like this, so we will.
+			int32_t	*data=(int32_t *)CMSG_DATA(cmptr);
+			*fd=*data;
 
 			delete[] control;
 			return true;
