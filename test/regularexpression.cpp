f48f81c4abaf93ad2b9e4d8768e42f8f491eda31
David Muse <david.muse@firstworks.com>
2013-09-25 06:25:06 +0000

    *** empty log message ***

--- test/regularexpression.cpp
+++ test/regularexpression.cpp
@@ -17,96 +17,136 @@ void printMatches(regularexpression *re) {
 int main(int argc, const char **argv) {
 
         // A quick match...
-        if (regularexpression::match("Hello!",".*Dave.*")) {
-                stdoutput.printf("The string contains .*Dave.*\n");
+        const char	*str="Hello!";
+        const char	*mtc="Dave";
+        if (regularexpression::match(str,mtc)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
         } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
         }
 
-        if (regularexpression::match("Hello Dave!",".*Dave.*")) {
-                stdoutput.printf("The string contains .*Dave.*\n");
+	str="Hello Dave!";
+        if (regularexpression::match(str,mtc)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
         } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+	}
 
         // If you need to match over and over...
-        regularexpression       re("Dave");
-        if (re.match("Hello!")) {
-                stdoutput.printf("The string contains Dave\n");
+        regularexpression       re(mtc);
+        str="Hello!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
         } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-        if (re.match("Hello Dave!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-	re.study();
-        if (re.match("Goodbye!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-        if (re.match("Goodbye Dave!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-	re.study();
-        if (re.match("He is a jerk!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-        if (re.match("Dave is a jerk!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-        if (re.match("He writes cool software!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-        if (re.match("Dave writes cool software!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-        if (re.match("See ya later Dave!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
-        }
-	printMatches(&re);
-
-        if (re.match("See ya later!")) {
-                stdoutput.printf("The string contains Dave\n");
-        } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
 	}
 	printMatches(&re);
 
-        if (re.match("Dave, Dave, Dave!")) {
-                stdoutput.printf("The string contains Dave\n");
+	str="Hello Dave!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
         } else {
-                stdoutput.printf("The string doesn't contain Dave\n");
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
 	}
+	printMatches(&re);
+
+	re.study();
+	str="Goodbye!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+	}
+	printMatches(&re);
+
+	str="Goodbye Dave!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+	}
+	printMatches(&re);
+
+	re.study();
+	str="He is a jerk!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+	}
+	printMatches(&re);
+
+	str="Dave is a jerk!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+	}
+	printMatches(&re);
+
+        str="He writes cool software!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+	}
+	printMatches(&re);
+
+        str="Dave writes cool software!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+        }
+	printMatches(&re);
+
+	str="See ya later Dave!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+        }
+	printMatches(&re);
+
+	str="See ya later!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+        }
+	printMatches(&re);
+
+	str="Dave, Dave, Dave!";
+        if (re.match(str)) {
+                stdoutput.printf(
+			"The string \"%s\" contains \"%s\"\n",str,mtc);
+        } else {
+                stdoutput.printf(
+			"The string \"%s\" doesn't contain \"%s\"\n",str,mtc);
+        }
 	printMatches(&re);
 }
