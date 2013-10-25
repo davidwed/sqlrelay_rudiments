0bebaf4e56a1ca485559abf80ab3daed7abc12ba
David Muse <david.muse@firstworks.com>
2013-10-25 10:14:51 +0000

    #1801 removed the clientserverfactory class

--- examples/clientserverfactory.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2012  David Muse
-// See the file COPYING for more information
-
-int main(int argc, const char **argv) {
-}
--- include/rudiments/private/clientserverfactoryincludes.h
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2002 David Muse
-// See the COPYING file for more information.
-
-#include <rudiments/private/dll.h>
-#include <rudiments/client.h>
-#include <rudiments/server.h>
