eaa25a31b0b041d197138a7a5e7ac40e4b70ba0f
David Muse <david.muse@firstworks.com>
2013-11-28 00:44:51 +0000

    *** empty log message ***

---
+++ include/rudiments/private/permissions.h
@@ -0,0 +1,8 @@
+// Copyright (c) 1999-2002 David Muse
+// See the COPYING file for more information.
+
+	static char	*permStringToSDDL(const char *permstring,
+							bool directory);
+
+	static char	*permOctalToSDDL(mode_t permoctal,
+						bool directory);
