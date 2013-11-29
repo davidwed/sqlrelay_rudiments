5625ca2ffcb768a7c6385294810911c4c6974672
David Muse <david.muse@firstworks.com>
2013-11-29 23:56:19 +0000

    #151 added a memorymap test

--- test/memorymap.cpp
+++ test/memorymap.cpp
@@ -1,9 +1,49 @@
 // Copyright (c) 2003  David Muse
 // See the file COPYING for more information
 
-#include <stdio.h>
+#include <rudiments/file.h>
+#include <rudiments/filesystem.h>
+#include <rudiments/memorymap.h>
+#include <rudiments/process.h>
+#include <rudiments/stdio.h>
+#include <rudiments/error.h>
 
 int main(int argc, const char **argv) {
 
-	// FIXME: implement this...
+	// open the file
+	file	f;
+	if (!f.open("xmls.xml",O_RDONLY)) {
+		stdoutput.printf("failed to open xmls.xml\n");
+		process::exit(1);
+	}
+
+	// open the filesystem to get the optimimum transfer block size
+	filesystem	fs;
+	if (!fs.initialize("xmls.xml")) {
+		stdoutput.printf("failed to open filesystem\n");
+		process::exit(1);
+	}
+
+	// loop, mapping and printing blocks of the file
+	memorymap	mm;
+	off64_t		offset=0;
+	size_t		len=fs.getOptimumTransferBlockSize();
+	const char	*ptr=NULL;
+	bool		done=false;
+	do {
+		if (offset) {
+			mm.detach();
+		}
+		if (offset+len>f.getSize()) {
+			len=f.getSize()-offset;
+			done=true;
+		}
+		if (!mm.attach(f.getFileDescriptor(),offset,len,
+						PROT_READ,MAP_PRIVATE)) {
+			break;
+		}
+		ptr=(const char *)mm.getData();
+		stdoutput.printf("%s",ptr);
+		offset=offset+len;
+	} while (!done);
 }
