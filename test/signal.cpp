06cfba07397b7d642d1e573a0ad6e9b8ae7a9c30
David Muse <david.muse@firstworks.com>
2014-02-22 04:50:15 +0000

    #2002 added sigterm test

--- test/signal.cpp
+++ test/signal.cpp
@@ -4,11 +4,18 @@
 #include <rudiments/signalclasses.h>
 #include <rudiments/stdio.h>
 
+bool	gotsigterm=false;
+
 #ifdef SIGFPE
 void handleSigfpe(int sig) {
 	stdoutput.printf("Got a SIGFPE!\n");
 }
 #endif
+
+void handleSigterm(int sig) {
+	stdoutput.printf("Got a SIGTERM!\n");
+	gotsigterm=true;
+}
 
 void handleAlarm(int sig) {
 	stdoutput.printf("alarm!\n");
@@ -16,21 +23,28 @@ void handleAlarm(int sig) {
 
 int main(int argc, const char **argv) {
 
-	// this program will ignore all signals except SIGFPE
+	// this program will ignore all signals except
+	// SIGFPE, SIGTERM and SIGALRM
 	signalset	ignoreset;
 	ignoreset.addAllSignals();
 	#ifdef SIGFPE
 	ignoreset.removeSignal(SIGFPE);
 	#endif
+	ignoreset.removeSignal(SIGTERM);
 	ignoreset.removeSignal(SIGALRM);
 	signalmanager::ignoreSignals(&ignoreset);
 
 	// when it gets a SIGFPE, it will run the handleSigfpe() function
 	#ifdef SIGFPE
-	signalhandler	shandler;
-	shandler.setHandler(handleSigfpe);
-	shandler.handleSignal(SIGFPE);
+	signalhandler	fpehandler;
+	fpehandler.setHandler(handleSigfpe);
+	fpehandler.handleSignal(SIGFPE);
 	#endif
+
+	// when it gets a SIGTERM, it will run the handleSigterm() function
+	signalhandler	termhandler;
+	termhandler.setHandler(handleSigterm);
+	termhandler.handleSignal(SIGTERM);
 
 	// handle alarm
 	signalhandler	alarmhandler;
@@ -41,7 +55,7 @@ int main(int argc, const char **argv) {
 	// to be sent. Since SIGFPE is the only signal not in the ignoreset,
 	// waitForSignals will fall through only when SIGFPE is received.
 	signalmanager::alarm(2);
-	for (;;) {
+	while (!gotsigterm) {
 		signalmanager::waitForSignals(&ignoreset);
 		signalmanager::alarm(2);
 	}
