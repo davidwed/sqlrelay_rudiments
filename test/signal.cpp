85463b7db1d84ba74230448b97289bae35e13f4a
David Muse <david.muse@firstworks.com>
2014-02-22 04:44:45 +0000

    #2002 added an alarm

--- test/signal.cpp
+++ test/signal.cpp
@@ -4,8 +4,14 @@
 #include <rudiments/signalclasses.h>
 #include <rudiments/stdio.h>
 
+#ifdef SIGFPE
 void handleSigfpe(int sig) {
 	stdoutput.printf("Got a SIGFPE!\n");
+}
+#endif
+
+void handleAlarm(int sig) {
+	stdoutput.printf("alarm!\n");
 }
 
 int main(int argc, const char **argv) {
@@ -13,18 +19,30 @@ int main(int argc, const char **argv) {
 	// this program will ignore all signals except SIGFPE
 	signalset	ignoreset;
 	ignoreset.addAllSignals();
+	#ifdef SIGFPE
 	ignoreset.removeSignal(SIGFPE);
+	#endif
+	ignoreset.removeSignal(SIGALRM);
 	signalmanager::ignoreSignals(&ignoreset);
 
 	// when it gets a SIGFPE, it will run the handleSigfpe() function
+	#ifdef SIGFPE
 	signalhandler	shandler;
 	shandler.setHandler(handleSigfpe);
 	shandler.handleSignal(SIGFPE);
+	#endif
+
+	// handle alarm
+	signalhandler	alarmhandler;
+	alarmhandler.setHandler(handleAlarm);
+	alarmhandler.handleSignal(SIGALRM);
 
 	// Loop forever, each time waiting for a signal not in the ignoreset
 	// to be sent. Since SIGFPE is the only signal not in the ignoreset,
 	// waitForSignals will fall through only when SIGFPE is received.
+	signalmanager::alarm(2);
 	for (;;) {
 		signalmanager::waitForSignals(&ignoreset);
+		signalmanager::alarm(2);
 	}
 }
