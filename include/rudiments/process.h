07fe43f595f09a475298031eb453bda3680267da
David Muse <david.muse@firstworks.com>
2014-03-07 19:03:14 +0000

    removed stubs for a bunch of unimplemented classes

--- include/rudiments/process.h
+++ include/rudiments/process.h
@@ -6,43 +6,6 @@
 
 #include <rudiments/private/processincludes.h>
 
-// wrap:
-//	unistd.h - nice()
-//			getgroups()
-//			vfork()
-//			profil()
-//			acct()
-//			getdtablesize()
-//			brk()/sbrk()
-//			syscall()
-//	sched.h - functions for manipulating the scheduler
-//			sched_setparam(),sched_getparam()
-//			sched_setscheduler(),sched_getscheduler()
-//			sched_yield(),
-//			sched_get_priority_max(),sched_get_priority_min()
-//			sched_rr_get_interval()
-//			sched_setaffinity(),sched_getaffinity()
-//	sys/acct.h - acct() - process accounting on/off
-//	sys/ptrace.h - ptrace()
-//	sys/resource.h - get/set process priority/resource usage
-//				getrlimit(),setrlimit()
-//				getrusage()
-//				getpriority(),setpriority()
-//	sys/times.h - times() - function for getting process times
-//	not in many systems - 
-//		unistd.h - group_member()
-//				getresuid(),getresgid()
-//				setresuid(),setresgid()
-//		sys/gmon.h and sys/gmon_out.h - monstartup()
-//	linux only - 
-//		execinfo.h - backtrace(),backtrace_symbols(),
-//				backtrace_symbols_fd()
-//		sys/prctl.h - prctl()
-//	not in bsd - 
-//		ucontext.h - user-level context switching
-//			getcontext(),setcontext(),swapcontext(),makecontext()
-//
-
 enum childstatechange {
 	EXIT_CHILDSTATECHANGE=0,
 	TERMINATED_CHILDSTATECHANGE,
