3f66c94c9dbb12db8185ab1fe562ef50a94efbc1
David Muse <david.muse@firstworks.com>
2014-10-06 17:00:19 +0000

    *** empty log message ***

--- test/crypt.cpp
+++ test/crypt.cpp
@@ -1,9 +1,33 @@
-// Copyright (c) 2003  David Muse
+// Copyright (c) 2014  David Muse
 // See the file COPYING for more information
 
-#include <stdio.h>
+#include <rudiments/crypt.h>
+#include <rudiments/stdio.h>
 
 int main(int argc, const char **argv) {
 
-	// FIXME: implement this...
+	const char	*salts[]={
+		"FW",
+		"fw",
+		NULL
+	};
+
+	const char	*strings[]={
+		"rudiments",
+		"RUDIMENTS",
+		"firstworks",
+		"FIRSTWORKS",
+		NULL
+	};
+
+	for (const char * const *salt=salts; *salt; salt++) {
+
+		stdoutput.printf("salt=%s\n",*salt);
+
+		for (const char * const *str=strings; *str; str++) {
+			char	*encrypted=crypt::encrypt(*str,*salt);
+			stdoutput.printf("  %s: %s\n",*str,encrypted);
+			delete[] encrypted;
+		}
+	}
 }
