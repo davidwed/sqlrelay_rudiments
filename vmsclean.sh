3927f48d9c34ab5a848e32fb522139fdaccb3a99
David Muse <david.muse@firstworks.com>
2015-04-20 15:44:57 +0000

    *** empty log message ***

--- vmsclean.sh
+++ vmsclean.sh
@@ -1,3 +1,2 @@
-rm *.vbs
-rm configure*
-rm -rf doc examples man msvc
+make distclean
+rm -rf *.vbs config* ac* ltmain.sh autogen.sh install-sh mkinstalldirs rudiments.pc.in rudiments.spec ChangeLog COPYING AUTHORS include/rudiments/private/config.*.h config.h.in bin doc examples man msvc test/certificates
