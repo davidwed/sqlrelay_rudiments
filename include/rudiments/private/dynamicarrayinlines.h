9ee2e4337a65e1f45647e6e476bbc4a357075d3d
David Muse <david.muse@firstworks.com>
2015-05-09 08:10:40 +0000

    #3150 added missing inline qualifiers

--- include/rudiments/private/dynamicarrayinlines.h
+++ include/rudiments/private/dynamicarrayinlines.h
@@ -1,25 +1,30 @@
 // Copyright (c) 2015 David Muse
 // See the COPYING file for more information.
 
+#include <rudiments/private/rudimentsinlines.h>
 #include <rudiments/private/new.h>
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 dynamicarray<valuetype>::dynamicarray() {
 	init(128,32);
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 dynamicarray<valuetype>::dynamicarray(uint64_t initialsize,
 						uint64_t increment) {
 	init((initialsize)?initialsize:128,(increment)?increment:32);
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 dynamicarray<valuetype>::dynamicarray(const dynamicarray<valuetype> &v) {
 	dynamicarrayClone(v);
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 dynamicarray<valuetype> &dynamicarray<valuetype>::operator=(
 					const dynamicarray<valuetype> &v) {
 	if (this!=&v) {
@@ -30,6 +35,7 @@ dynamicarray<valuetype> &dynamicarray<valuetype>::operator=(
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 void dynamicarray<valuetype>::init(uint64_t initialsize,
 						uint64_t increment) {
 	size=0;
@@ -42,6 +48,7 @@ void dynamicarray<valuetype>::init(uint64_t initialsize,
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 void dynamicarray<valuetype>::dynamicarrayClone(
 				const dynamicarray<valuetype> &v) {
 
@@ -63,11 +70,13 @@ void dynamicarray<valuetype>::dynamicarrayClone(
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 dynamicarray<valuetype>::~dynamicarray() {
 	clearExtentList();
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 valuetype &dynamicarray<valuetype>::operator[](uint64_t index) {
 	extend(index+1);
 	if (index>=len) {
@@ -77,11 +86,13 @@ valuetype &dynamicarray<valuetype>::operator[](uint64_t index) {
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 uint64_t dynamicarray<valuetype>::getLength() const {
 	return len;
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 void dynamicarray<valuetype>::extend(uint64_t size) {
 	uint64_t	inc=(extents.getLength())?extsize:initial;
 	while (this->size<size) {
@@ -93,6 +104,7 @@ void dynamicarray<valuetype>::extend(uint64_t size) {
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 valuetype &dynamicarray<valuetype>::find(uint64_t index) {
 
 	// move to the extent that contains the specified index
@@ -121,6 +133,7 @@ valuetype &dynamicarray<valuetype>::find(uint64_t index) {
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 void dynamicarray<valuetype>::clearExtentList() {
 	curext=extents.getFirst();
 	while (curext) {
@@ -133,6 +146,7 @@ void dynamicarray<valuetype>::clearExtentList() {
 }
 
 template< class valuetype >
+RUDIMENTS_TEMPLATE_INLINE
 void dynamicarray<valuetype>::clear() {
 
 	// remove all but the first extent
--- include/rudiments/private/staticarrayinlines.h
+++ include/rudiments/private/staticarrayinlines.h
@@ -1,15 +1,18 @@
 // Copyright (c) 2015 David Muse
 // See the COPYING file for more information.
 
+#include <rudiments/private/rudimentsinlines.h>
 #include <rudiments/private/new.h>
 
 template< class valuetype, uint64_t length >
+RUDIMENTS_TEMPLATE_INLINE
 staticarray<valuetype,length>::staticarray() {
 	len=length;
 	data=new valuetype[len];
 }
 
 template< class valuetype, uint64_t length >
+RUDIMENTS_TEMPLATE_INLINE
 staticarray<valuetype,length>::staticarray(const staticarray<valuetype,length> &v) {
 	len=v.len;
 	data=new valuetype[len];
@@ -19,6 +22,7 @@ staticarray<valuetype,length>::staticarray(const staticarray<valuetype,length> &
 }
 
 template< class valuetype, uint64_t length >
+RUDIMENTS_TEMPLATE_INLINE
 staticarray<valuetype,length> &staticarray<valuetype,length>::operator=(
 				const staticarray<valuetype,length> &v) {
 	if (this!=&v) {
@@ -32,21 +36,25 @@ staticarray<valuetype,length> &staticarray<valuetype,length>::operator=(
 }
 
 template< class valuetype, uint64_t length >
+RUDIMENTS_TEMPLATE_INLINE
 staticarray<valuetype,length>::~staticarray() {
 	delete[] data;
 }
 
 template< class valuetype, uint64_t length >
+RUDIMENTS_TEMPLATE_INLINE
 valuetype &staticarray<valuetype,length>::operator[](uint64_t index) {
 	return data[index];
 }
 
 template< class valuetype, uint64_t length >
+RUDIMENTS_TEMPLATE_INLINE
 uint64_t staticarray<valuetype,length>::getLength() const {
 	return len;
 }
 
 template< class valuetype, uint64_t length >
+RUDIMENTS_TEMPLATE_INLINE
 void staticarray<valuetype,length>::clear() {
 	for (uint64_t i=0; i<len; i++) {
 		((valuetype *)&data[i])->~valuetype();
