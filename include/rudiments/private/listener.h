b8a3c10f41d5a2202b06750b371b84846a645e18
David Muse <david.muse@firstworks.com>
2015-05-30 00:54:54 +0000

    *** empty log message ***

--- include/rudiments/private/listener.h
+++ include/rudiments/private/listener.h
@@ -1,12 +1,6 @@
 // Copyright (c) 2002 David Muse
 // See the COPYING file for more information.
 
-	protected:
-
-		// FIXME: it should be possible to copy a listener
-				listener(const listener &l);
-		listener	&operator=(const listener &l);
-
 	private:
 		void	addFileDescriptor(filedescriptor *fd,
 						bool read, bool write);
