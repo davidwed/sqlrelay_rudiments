720b208bb22ce306450642e983c28cc8e168a705
David Muse <david.muse@firstworks.com>
2015-08-27 17:46:10 +0000

    #3473 added bench skeleton

---
+++ bench/map.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2015  David Muse
+// See the file COPYING for more information
+
+#include <map>
+#include <rudiments/datetime.h>
+#include <rudiments/stdio.h>
+
+int main(int argc, char **argv) {
+}
