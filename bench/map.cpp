8408e2f03cf81057640aa44623c4ad8847ab6015
David Muse <david.muse@firstworks.com>
2015-08-28 04:06:57 +0000

    *** empty log message ***

--- bench/map.cpp
+++ bench/map.cpp
@@ -1,9 +1,5 @@
 // Copyright (c) 2015  David Muse
 // See the file COPYING for more information
 
-#include <map>
-#include <rudiments/datetime.h>
-#include <rudiments/stdio.h>
-
-int main(int argc, char **argv) {
-}
+#define USE_MAP
+#include "maps.cpp"
