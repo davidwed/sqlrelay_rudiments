162c422ac6256bb9746e332e418b87849059f4bd
David Muse <david.muse@firstworks.com>
2016-11-18 06:56:05 +0000

    *** empty log message ***

--- configure.com
+++
@@ -1,2 +0,0 @@
-$ del [.include.rudiments.private]config.h
-$ copy [.include.rudiments.private]config_openvms721.h [.include.rudiments.private]config.h
--- vmsclean.sh
+++
@@ -1,2 +0,0 @@
-make distclean
-rm -rf *.vbs config.guess config.mk.in config.sub configure configure.in configure.vbs config.windows.mk aclocal.m4 acsite.m4 ltmain.sh autogen.sh install-sh mkinstalldirs rudiments.pc.in rudiments.spec ChangeLog COPYING AUTHORS include/rudiments/private/config.*.h include/rudiments/private/config.h.in bin doc examples man msvc test/certificates
