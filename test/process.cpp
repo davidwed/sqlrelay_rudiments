d663373b943a496f5ba7318236639fff16382d36
David Muse <david.muse@firstworks.com>
2016-12-04 06:58:50 +0000

    *** empty log message ***

--- test/process.cpp
+++ test/process.cpp
@@ -40,10 +40,12 @@ int main(int argc, const char **argv) {
 
 	test("pid",pid>1);
 	test("ppid",ppid>1);
+#ifndef _WIN32
 	test("pgid",pgid==pid);
 	test("sid",sid==ppid);
 	test("uid/euid",uid==euid);
 	test("gid/egid",gid==egid);
+#endif
 	test("create pidfile",process::createPidFile("pidfile",
 				permissions::evalPermString("rw-r--r--")));
 	test("check pidfile",process::checkForPidFile("pidfile"));
