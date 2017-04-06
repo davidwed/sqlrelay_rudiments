5bdb85483369b7d8b8c98687387cfd0b747b3527
David Muse <david.muse@firstworks.com>
2017-04-06 12:45:14 +0000

    *** empty log message ***

--- test/certificates/sign.sh
+++ test/certificates/sign.sh
@@ -6,5 +6,16 @@ then
 	exit 1
 fi
 
+PEM=`basename $1 .csr`.pem
+
+if ( test -r "$PEM" )
+then
+	openssl ca -config config/ca.cnf -revoke $PEM
+	if ( test "$?" != "0" )
+	then
+		echo "revoke failed"
+		exit 1
+	fi
+fi
 export OPENSSL_ENABLE_MD5_VERIFY=1
-openssl ca -batch -config config/ca.cnf -in $1 -out `basename $1 .csr`.pem
+openssl ca -batch -config config/ca.cnf -in $1 -out $PEM
